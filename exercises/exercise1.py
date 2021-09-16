6e481fd1357e187ccd15934c82a41d7931623a54
AsBeeb <asbeeb@gmail.com>
2021-09-16 14:35:39 +0200

    Update exercise1.py

--- exercises/exercise1.py
+++ exercises/exercise1.py
@@ -1,7 +1,7 @@
 from emulators.Device import Device
 from emulators.Medium import Medium
 from emulators.MessageStub import MessageStub
-
+import random
 
 class GossipMessage(MessageStub):
 
@@ -15,7 +15,6 @@ class GossipMessage(MessageStub):
 
 
 class Gossip(Device):
-
     def __init__(self, index: int, number_of_devices: int, medium: Medium):
         super().__init__(index, number_of_devices, medium)
         # for this exercise we use the index as the "secret", but it could have been a new routing-table (for instance)
@@ -23,9 +22,33 @@ class Gossip(Device):
         self._secrets = set([index])
 
     def run(self):
-        # the following is your termination condition, but where should it be placed?
-        # if len(self._secrets) == self.number_of_devices():
-        #    return
+        while True:
+            # in each repetition, let us send the ping to one random other device
+            message = GossipMessage(self.index(), random.randint(0, self.number_of_devices()), self._secrets)
+            
+            if self.index() == message.destination:
+                print("DONT SEND From: ", self.index(), "To: ", message.destination)
+                continue
+            print("From: ", self.index(), "To: ", message.destination)
+
+            # we send the message via a "medium"
+            self.medium().send(message)
+
+            # in this instance, we also try to receive some messages, there can be multiple, but
+            # eventually the medium will return "None"
+            ingoing = self.medium().receive()
+            if ingoing is None:
+                break
+
+            # let's keep some statistics
+            self._secrets.add(ingoing.secrets)
+
+            # this call is only used for synchronous networks
+            self.medium().wait_for_next_round()
+
+            # the following is your termination condition, but where should it be placed?
+            if len(self._secrets) == self.number_of_devices():
+                return
         return
 
     def print_result(self):
